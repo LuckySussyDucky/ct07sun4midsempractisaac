181c5a04b6cb4ed7c248438babcc7988fe03f513
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-31 08:43:08 +0000

    isaac fuinal answewr

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
+let ball;
 
+function setup(){
+    new Canvas(600, 400);
+    world.gravity.y = 10;
+}
+
+function draw(){
+    background(250)
+    if(mouse.presses("left")){
+        ball = new Sprite(mouse.x, mouse.y, 25, "dynamic")
+    }
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let ball;
+let floor;
+
+function setup(){
+    new Canvas(600, 400);
+    world.gravity.y = 10;
+    floor = new Sprite(300, 400, 600, 30, "static");
+    floor.color = "red";
+}
+
+function draw(){
+    background(250)
+    if(mouse.presses("left") ||mouse.presses("right")){
+        ball = new Sprite(mouse.x, mouse.y, 25, "dynamic")
+        ball.bounciness = 0.5;
+    }
+}
 
